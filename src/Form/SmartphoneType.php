2befeed6cf6ca8a3253a44b323938969acfba51d
remicorreia <remi.correia@lepoles.org>
2023-02-28 15:26:31 +0100

    debut mise en place aff smart

--- src/Form/SmartphoneType.php
+++ src/Form/SmartphoneType.php
@@ -4,20 +4,32 @@ namespace App\Form;
 
 use App\Entity\Smartphone;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class SmartphoneType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('photo')
-            ->add('nom')
-            ->add('description')
-            ->add('prix')
-        ;
-    }
+        $builder->add('photo', FileType::class, [
+            "mapped" => false,
+            "required" => false,
+            "constraints" => [
+                new File([
+                    "mimeTypes"         => [ "image/gif", "image/jpeg", "image/png" ],
+                    "mimeTypesMessage"  => "Les formats autorisés sont gif, jpg, png",
+                    "maxSize"           => "8Mi",
+                    "maxSizeMessage"    => "Le fichier ne peut pas peser plus de 2Mo"
+                ])
+            ]
+        ])
+        ->add('nom')
+        ->add('description')
+        ->add('prix')
+    ;
+}
 
     public function configureOptions(OptionsResolver $resolver): void
     {
