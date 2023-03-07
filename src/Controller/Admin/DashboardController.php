5d750985f801d0b1b256c58358e4ec59acacdf67
remicorreia <remi.correia@lepoles.org>
2023-03-07 16:53:40 +0100

    navV4

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -42,6 +42,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Smartphone', 'fa-solid fa-file-lines', Smartphone ::class);
+        yield MenuItem::linkToCrud('Smartphone', 'fa-solid fa-mobile', Smartphone ::class);
     }
 }
